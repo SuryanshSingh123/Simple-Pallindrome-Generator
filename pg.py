7e06fdeea762d36cf72b84d86d3b89ba16ace36a
Suryansh Singh <99817718+SuryanshSingh123@users.noreply.github.com>
2026-05-28 10:04:34 +0530

    Upload main file

---
+++ pg.py
@@ -0,0 +1,8 @@
+
+a = int(input("Enter amount of numbers to be generated: "))
+for i in range(1, a+1):
+    for j in range(1, i+1):
+        print(j, end="")
+    for k in range(i-1, 0, -1):
+        print(k, end="")
+    print()
